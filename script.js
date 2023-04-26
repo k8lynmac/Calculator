903b333161397a31c671531bf73eae900a149169
Katelyn <kcm34@pitt.edu>
2023-04-26 11:00:59 -0700

    Calc functions working

--- script.js
+++ script.js
@@ -1,37 +1,61 @@
+// globals
+let solution = 0;
+let dispValue = 0;
+let operation = "";
 let num = "";
+let opCount=0;
+let eqCount = 0;
+let storeEqual = 0;
+
+initializeNumberButtons();
+initializeOperator();
+initializeEqual(); 
+initializeDeletes();
 
 //event listener for digit (0-9) buttons
 function initializeNumberButtons() {
-    //an array of length 10 for numbers 0-9
     const digits = document.querySelectorAll(".digit");
-    
-
-    //iterating through each position and listening for user click
-    digits.forEach(digit => {
-        digit.addEventListener("click", function() {
-            num = num + digit.id;
-            //displays the number as the user clicks 
-            document.getElementById("calcdisplay").innerHTML = num; 
+        digits.forEach(digit => {
+            digit.addEventListener("click", function() {
+                num = num + digit.id;
+                
+                //displays the number as the user clicks 
+                document.getElementById("calcdisplay").innerHTML = num; 
         });
     });
 }
 
-let dispValue = 0;
-let operation = "";
-
 //event listener for operator buttons
 function initializeOperator() {
-    //an array of length 4 for operators
     const operations = document.querySelectorAll(".operator");
 
-    //iterating through each position and listening for user click
     operations.forEach(operator => {
-        operator.addEventListener("click", function() { 
-            dispValue = parseInt(num);
-            operation = operator.id;
-            console.log(dispValue + operation);
-            document.getElementById("calchistory").innerHTML = dispValue + " " + operator.id;
-            num="";
+        operator.addEventListener("click", function() {
+            
+            if (opCount === 0) {
+                dispValue = parseInt(num);
+                operation = operator.id;
+                document.getElementById("calchistory").innerHTML = dispValue + " " + operation;
+                num="";
+                opCount ++;
+            }
+            else if (opCount > 0 && eqCount > 0) {
+                
+                operation = operator.id;
+                document.getElementById("calchistory").innerHTML = storeEqual + " " + operation;
+                document.getElementById("calcdisplay").innerHTML = ""; 
+                dispValue = storeEqual;
+                num = "";
+                eqCount = 0;
+            }
+             else {
+                solution = operate(operation,  parseFloat(dispValue), parseFloat(num));
+                operation = operator.id;
+                document.getElementById("calchistory").innerHTML = solution + " " + operation;
+                document.getElementById("calcdisplay").innerHTML = ""; 
+                dispValue = solution;
+                num = "";
+            }
         });
     });
 }
@@ -40,11 +64,13 @@ function initializeOperator() {
 function initializeEqual() {
     const equals = document.querySelector(".equals");
 
-    //listening for user click
     equals.addEventListener("click", function() {
-            let solution = operate(operation, dispValue, parseInt(num)); 
+            solution = operate(operation, parseFloat(dispValue), parseFloat(num));
             document.getElementById("calchistory").innerHTML = dispValue + " " + operation + " " + num + " ="; 
             document.getElementById("calcdisplay").innerHTML = solution; 
+            num = "";
+            storeEqual = solution; 
+            eqCount ++;
         });
 }
 
@@ -52,25 +78,22 @@ function initializeEqual() {
 function initializeDeletes() {
     const removes = document.querySelectorAll(".bigbutton");
 
-     //iterating through each position and listening for user click
      removes.forEach(removal => {
         removal.addEventListener("click", function() {
             document.getElementById("calchistory").innerHTML = "";
             document.getElementById("calcdisplay").innerHTML = ""; 
-            num="";
+            num = "";
+            dispValue = 0;
+            digCount = 0;
+            opCount = 0;
+            eqCount = 0;
         });
     });
 }
 
-initializeNumberButtons();
-initializeOperator();
-initializeEqual(); 
-initializeDeletes();
-
 //takes an operator and 2 numbers and calls one of the above functions
 function operate(operation, first, second) {
     let result = 0;
-    console.log(first + " " + operation + " " + second);
 
     if (operation === "+") {
         result = first + second;
@@ -82,7 +105,14 @@ function operate(operation, first, second) {
         result = first * second;
     }
     else if (operation === "/") {
+        if (second === 0) {
+            alert("Dividing by 0 returns undefined result");
+            return;
+        }
         result = first / second;
+        result = result.toFixed(2);
     }
+ 
+    console.log(first + " " + operation + " " + second + " = " + result);
     return result; 
 }
